bfd23afe94f0391c144731d7271dd6585375ef9c
tamil-hash <ts795580@gmail.com>
2021-08-19 14:55:45 +0530

    added seperate sections for arithmetic operations in history

--- script.js
+++ script.js
@@ -5,19 +5,43 @@ const deleteBtn = document.querySelector(".del-btn");
 const btns = document.querySelectorAll(".calc-btn");
 const alert = document.querySelector(".alert");
 const history = document.querySelector(".history");
+const calSections = document.querySelectorAll(".cal");
 const historyBtn = document.querySelector(".history-btn");
 
-const previousCalculations =
-  JSON.parse(localStorage.getItem("previousCalculations")) || [];
+console.log(calSections);
+
+const previousCalculations = JSON.parse(
+  localStorage.getItem("previousCalculations")
+) || {
+  mixed: [],
+  addition: [],
+  subtraction: [],
+  multiplication: [],
+  division: [],
+  modulus: [],
+};
 
 //retreiving history from local Storage
 
 window.addEventListener("DOMContentLoaded", () => {
-  if (previousCalculations.length !== 0) {
-    previousCalculations.forEach((calculation) => {
-      setHistory(calculation);
-    });
-  }
+  previousCalculations.mixed.forEach((calculation) => {
+    setItem(0, calculation);
+  });
+  previousCalculations.addition.forEach((calculation) => {
+    setItem(1, calculation);
+  });
+  previousCalculations.subtraction.forEach((calculation) => {
+    setItem(2, calculation);
+  });
+  previousCalculations.multiplication.forEach((calculation) => {
+    setItem(3, calculation);
+  });
+  previousCalculations.division.forEach((calculation) => {
+    setItem(4, calculation);
+  });
+  previousCalculations.modulus.forEach((calculation) => {
+    setItem(5, calculation);
+  });
 });
 
 //calculator
@@ -45,13 +69,8 @@ equalBtn.addEventListener("click", () => {
   } else {
     const answer = eval(display.value);
     const calculation = display.value + "=" + answer;
-    display.value = answer;
-    previousCalculations.push(calculation);
     setHistory(calculation);
-    localStorage.setItem(
-      "previousCalculations",
-      JSON.stringify(previousCalculations)
-    );
+    display.value = answer;
   }
 });
 
@@ -72,8 +91,68 @@ closeBtn.addEventListener("click", () => {
 //set history
 
 function setHistory(calculation) {
+  let index;
+  if (
+    (calculation.indexOf("+") !== -1 && calculation.indexOf("-") !== -1) ||
+    (calculation.indexOf("+") !== -1 && calculation.indexOf("*") !== -1) ||
+    (calculation.indexOf("+") !== -1 && calculation.indexOf("/") !== -1) ||
+    (calculation.indexOf("+") !== -1 && calculation.indexOf("%") !== -1) ||
+    (calculation.indexOf("-") !== -1 && calculation.indexOf("+") !== -1) ||
+    (calculation.indexOf("-") !== -1 && calculation.indexOf("*") !== -1) ||
+    (calculation.indexOf("-") !== -1 && calculation.indexOf("/") !== -1) ||
+    (calculation.indexOf("-") !== -1 && calculation.indexOf("%") !== -1) ||
+    (calculation.indexOf("*") !== -1 && calculation.indexOf("+") !== -1) ||
+    (calculation.indexOf("*") !== -1 && calculation.indexOf("-") !== -1) ||
+    (calculation.indexOf("*") !== -1 && calculation.indexOf("/") !== -1) ||
+    (calculation.indexOf("*") !== -1 && calculation.indexOf("%") !== -1) ||
+    (calculation.indexOf("/") !== -1 && calculation.indexOf("+") !== -1) ||
+    (calculation.indexOf("/") !== -1 && calculation.indexOf("-") !== -1) ||
+    (calculation.indexOf("/") !== -1 && calculation.indexOf("*") !== -1) ||
+    (calculation.indexOf("/") !== -1 && calculation.indexOf("%") !== -1) ||
+    (calculation.indexOf("%") !== -1 && calculation.indexOf("+") !== -1) ||
+    (calculation.indexOf("%") !== -1 && calculation.indexOf("-") !== -1) ||
+    (calculation.indexOf("%") !== -1 && calculation.indexOf("*") !== -1) ||
+    (calculation.indexOf("%") !== -1 && calculation.indexOf("/") !== -1)
+  ) {
+    index = 0;
+    setLS("mixed", calculation);
+  } else if (calculation.indexOf("+") === 1) {
+    index = 1;
+    setLS("addition", calculation);
+  } else if (calculation.indexOf("-") === 1) {
+    index = 2;
+    setLS("subtraction", calculation);
+  } else if (calculation.indexOf("*") === 1) {
+    index = 3;
+    setLS("multiplication", calculation);
+  } else if (calculation.indexOf("/") === 1) {
+    index = 4;
+    setLS("division", calculation);
+  } else if (calculation.indexOf("%") === 1) {
+    index = 5;
+    setLS("modulus", calculation);
+  }
   const element = document.createElement("h2");
   element.textContent = calculation;
   element.classList.add("calculation");
-  history.appendChild(element);
+  calSections[index].appendChild(element);
+}
+
+// localStorage
+
+function setLS(action, calculation) {
+  previousCalculations[action].push(calculation);
+  console.log(previousCalculations);
+
+  localStorage.setItem(
+    "previousCalculations",
+    JSON.stringify(previousCalculations)
+  );
+}
+
+function setItem(index, calculation) {
+  const element = document.createElement("h2");
+  element.textContent = calculation;
+  element.classList.add("calculation");
+  calSections[index].appendChild(element);
 }
